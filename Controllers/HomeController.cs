21650b275eb727439480a24e170ad74b83ea5817
Zach <zacharyhartt19@gmail.com>
2023-08-15 21:10:03 -0400

    Specify HTTP Verbs.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,12 +13,13 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [HttpGet, ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
