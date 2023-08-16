2a85724ed2cc222956e44807a0b46e1b125dfbce
Zach <zacharyhartt19@gmail.com>
2023-08-15 21:19:50 -0400

    Remove Logger from HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,12 +6,7 @@ namespace Portfolio.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    public HomeController() {}
 
     [HttpGet]
     public IActionResult Index()
